7d7dfec48b18dcf98e15be3c59913ed88d96c93b
mef1991fonseca <mef1991.fonseca@gmail.com>
2023-01-13 17:38:37 -0300

    primera version

---
+++ src/public/index.js
@@ -0,0 +1,45 @@
+console.log("javascript funcionando");
+
+//configuracion del socket del lado del cliente
+const socketClient = io();
+
+let user;
+
+Swal.fire({
+    title:"Hola usuario",
+    text:"bienvenido, ingresa tu usario",
+    input:"text",
+    allowOutsideClick:false
+}).then(respuesta=>{
+    // console.log(respuesta)
+    user = respuesta.value;
+});
+
+const campo = document.getElementById("messageField")
+
+campo.addEventListener("keydown",(evt)=>{
+    console.log(evt.key)
+    if(evt.key === "Enter"){
+        socketClient.emit("message",{
+            username:user,
+            message:campo.value
+        })
+    }
+})
+
+const messageContainer = document.getElementById("messageContainer");
+
+socketClient.on("historico",(data)=>{
+    let elementos="";
+    data.forEach(item=>{
+        elementos = elementos + `<p><strong>${item.username}</strong>: ${item.message}</p>`;
+    });
+    messageContainer.innerHTML = elementos;
+})
+
+socketClient.on("newUser",()=>{
+    Swal.fire({
+        text:"Nuevo usuario conectado",
+        toast:true
+    })
+})
